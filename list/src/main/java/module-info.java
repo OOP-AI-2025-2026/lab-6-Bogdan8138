d6e51271c00b28e3628a41b80095c866e7f93a23
Bogdan8138 <9127167@stud.op.edu.ua>
2025-10-24 13:05:42 +0300

    Update module-info.java

--- list/src/main/java/module-info.java
+++ list/src/main/java/module-info.java
@@ -2,8 +2,6 @@ module ua.opnu.list {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-
     opens ua.opnu.list to javafx.fxml;
     exports ua.opnu.list;
 }
