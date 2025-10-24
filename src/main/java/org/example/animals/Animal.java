abe5e9863bc90f0cee07fc53a05ecef87457303a
Bogdan8138 <9127167@stud.op.edu.ua>
2025-10-24 12:58:39 +0300

    Add abstract Animal class with name property

---
+++ src/main/java/org/example/animals/Animal.java
@@ -0,0 +1,11 @@
+package org.example.animals;
+
+public abstract class Animal {
+    private String name;
+
+    public Animal(String name) { this.name = name; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public abstract String makeSound(); 
+}
